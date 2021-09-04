e7cfbf3cc93f7082969912f8bd22263557963eab
Francesco Metta <fra.metta@hotmail.it>
2021-09-04 21:40:41 +0200

    Create fxmanifest.lua

---
+++ p1n-mafias/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts  {
+    '@es_extended/imports.lua',
+    '@es_extended/locale.lua',
+    'locales/*.lua',
+    'config.lua',
+}
+
+client_scripts  {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
