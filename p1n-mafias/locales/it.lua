39153cf973f89c58b79c6d1e2b2efb30995db6ee
Francesco Metta <fra.metta@hotmail.it>
2021-09-04 21:40:23 +0200

    added italian language file

---
+++ p1n-mafias/locales/it.lua
@@ -0,0 +1,33 @@
+Locales['it'] = {
+    -- Client Side
+    ['inventory'] = 'Inventario',
+    ['boss_menu'] = 'Menu del Capo',
+    ['open_mafia'] = 'Premi ~r~E~w~ per aprire il menu',
+    ['mafia_menu'] = 'Mafia Menu',
+    ['recruit_menu'] = 'Recluta',
+    ['fire_menu'] = 'Licenzia',
+    ['promote_menu'] = 'Promuovi',
+    ['descend_menu'] = 'Degrada',
+    ['not_boss'] = 'Non sei il capo!',
+    ['noplayersnearby'] = 'Nessun giocatore nelle vicinanze',
+
+    -- Server side
+    ['gov_auth'] = 'Devi richiedere l\'autorizzazione al Governo',
+    ['no_perm'] = 'Non hai il permesso!',
+
+    -- Server side promotion
+    ['promotion'] = 'Hai promosso %s',
+    ['promoted'] = 'Sei stato promosso da %s',
+
+    -- Server side degrade
+    ['degrade'] = 'Hai degradato %s',
+    ['degraded'] = 'Sei stato degradato da %s',
+
+    -- Server side recruit
+    ['recruit'] = 'Hai assunto %s',
+    ['recruited'] = 'Sei stato assunto da %s',
+
+    -- Server side fire
+    ['fire'] = 'Hai licenziato %s',
+    ['fired'] = 'Sei stato licenziato da %s',
+}
