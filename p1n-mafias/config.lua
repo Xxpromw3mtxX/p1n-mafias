66c9576ab951ba06f3f703ceb21f2613348fee2f
Francesco Metta <fra.metta@hotmail.it>
2021-09-04 21:40:35 +0200

    new config file

---
+++ p1n-mafias/config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+Config.Locale = 'it'
+
+Config.mafias = {
+    --[[{
+        Name = 'Mafia1',
+        job = 'police',
+        point = vector3(-803.833, 184.6375, 72.605),
+    },]]
+
+    -- Template for adding more gangs/mafias
+    --[[{
+        Name = 'Mafia1',
+        job = 'police',
+        point = vector3(-803.833, 184.6375, 72.605),
+    },]]
+}
