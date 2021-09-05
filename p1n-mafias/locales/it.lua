d7fe3e35f5e0964fc63796c6d8260336cd7262c7
Francesco Metta <fra.metta@hotmail.it>
2021-09-05 11:48:18 +0200

    Simplified it language file

--- p1n-mafias/locales/it.lua
+++ p1n-mafias/locales/it.lua
@@ -1,33 +1,17 @@
 Locales['it'] = {
-    -- Client Side
+    -- Open notification
+    ['open_mafia'] = 'Premi ~r~E~w~ per aprire il menu del capo',
+    ['open_inv'] = 'Premi ~r~E~w~ per aprire l\'inventario',
+    ['open_cloak'] = 'Premi ~r~E~w~ per aprire il guardaroba',
+
+    -- Inventory
     ['inventory'] = 'Inventario',
-    ['boss_menu'] = 'Menu del Capo',
-    ['open_mafia'] = 'Premi ~r~E~w~ per aprire il menu',
-    ['mafia_menu'] = 'Mafia Menu',
-    ['recruit_menu'] = 'Recluta',
-    ['fire_menu'] = 'Licenzia',
-    ['promote_menu'] = 'Promuovi',
-    ['descend_menu'] = 'Degrada',
+    ['cloakroom'] = 'Guardaroba',
+
+    -- Cloakroom
+    ['player_clothes'] = 'Vestiti personali',
+    ['remove_cloth'] = 'Rimuovi outfit',
+
+    -- Boss related string
     ['not_boss'] = 'Non sei il capo!',
-    ['noplayersnearby'] = 'Nessun giocatore nelle vicinanze',
-
-    -- Server side
-    ['gov_auth'] = 'Devi richiedere l\'autorizzazione al Governo',
-    ['no_perm'] = 'Non hai il permesso!',
-
-    -- Server side promotion
-    ['promotion'] = 'Hai promosso %s',
-    ['promoted'] = 'Sei stato promosso da %s',
-
-    -- Server side degrade
-    ['degrade'] = 'Hai degradato %s',
-    ['degraded'] = 'Sei stato degradato da %s',
-
-    -- Server side recruit
-    ['recruit'] = 'Hai assunto %s',
-    ['recruited'] = 'Sei stato assunto da %s',
-
-    -- Server side fire
-    ['fire'] = 'Hai licenziato %s',
-    ['fired'] = 'Sei stato licenziato da %s',
 }
